53a2bbfab005f2b30eb57cf4423bfe90e59f8a5d
Jerod Bury <jbury@kochava.com>
2016-10-14 12:36:23 -0700

    Basic working pipeline

--- kafka.go
+++ kafka.go
@@ -20,9 +20,14 @@ func GetKafkaConsumer(brokers []string) (sarama.Consumer, error) {
 
 	consumerMetricRegistry := metrics.NewPrefixedChildRegistry(appMetricRegistry, "consumer.")
 
-	config.MetricRegistry = appMetricRegistry
+	config.MetricRegistry = consumerMetricRegistry
 
-	go metrics.Log(consumerMetricRegistry, 30*time.Second, log.New(os.Stdout, "consumer: ", log.Lmicroseconds))
+	file, err := os.OpenFile("consumer.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file:", err)
+	}
+
+	go metrics.Log(consumerMetricRegistry, 30*time.Second, log.New(file, "consumer: ", log.Lmicroseconds))
 
 	// Create new consumer
 	return sarama.NewConsumer(brokers, config)
@@ -30,39 +35,75 @@ func GetKafkaConsumer(brokers []string) (sarama.Consumer, error) {
 }
 
 // GetKafkaProducer returns a new consumer
-func GetKafkaProducer(brokers []string) (sarama.AsyncProducer, error) {
+func GetKafkaProducer(brokers []string) (sarama.SyncProducer, error) {
 	config := sarama.NewConfig()
-	config.Producer.Retry.Max = 5
 
-	config.Producer.RequiredAcks = sarama.WaitForLocal
+	config.Producer.Retry.Max = 5
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewManualPartitioner
+	config.ClientID = "firehose"
 
 	appMetricRegistry := metrics.NewRegistry()
 
-	consumerMetricRegistry := metrics.NewPrefixedChildRegistry(appMetricRegistry, "consumer.")
+	producerMetricRegistry := metrics.NewPrefixedChildRegistry(appMetricRegistry, "producer.")
 
-	config.MetricRegistry = appMetricRegistry
+	config.MetricRegistry = producerMetricRegistry
 
-	go metrics.Log(consumerMetricRegistry, 30*time.Second, log.New(os.Stdout, "consumer: ", log.Lmicroseconds))
+	file, err := os.OpenFile("producer.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file:", err)
+	}
+
+	go metrics.Log(producerMetricRegistry, 30*time.Second, log.New(file, "producer: ", log.Lmicroseconds))
 
 	// Create new consumer
-	return sarama.NewConsumer(brokers, config)
+	return sarama.NewSyncProducer(brokers, config)
 
 }
 
 // PullFromTopic pulls messages from the topic partition
-func PullFromTopic(consumer sarama.PartitionConsumer, producer chan<- string, signals chan os.Signal, wg *sync.WaitGroup) {
+func PullFromTopic(consumer sarama.PartitionConsumer, producer chan<- *sarama.ProducerMessage, signals chan os.Signal, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for {
 		if len(signals) > 0 {
-			fmt.Println("Interrupt is detected")
+			fmt.Println("Consumer - Interrupt is detected - exiting")
 			return
 		}
 		select {
 		case err := <-consumer.Errors():
 			fmt.Println(err)
 			return
-		case <-consumer.Messages():
+		case consumerMsg := <-consumer.Messages():
+			producerMsg := &sarama.ProducerMessage{
+				Topic:     consumerMsg.Topic,
+				Partition: consumerMsg.Partition,
+				Key:       sarama.StringEncoder(consumerMsg.Key),
+				Value:     sarama.StringEncoder(consumerMsg.Value),
+			}
+			producer <- producerMsg
+		}
+	}
+}
+
+// PushToTopic pushes messages to topic
+func PushToTopic(producer sarama.SyncProducer, consumer <-chan *sarama.ProducerMessage, signals chan os.Signal, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		if len(signals) > 0 {
+			fmt.Println("Producer - Interrupt is detected - exiting")
+			return
+		}
+		select {
+		case consumerMsg := <-consumer:
+			_, _, err := producer.SendMessage(consumerMsg)
+			if err != nil {
+				log.Fatalln("Failed to produce message to kafka cluster.")
+				return
+			}
+
+			//fmt.Printf("Produced message to partition %d with offset %d\n", partition, offset)
 		}
 	}
 }
@@ -78,4 +119,22 @@ func TestProducer(partition int, consumer <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	cPartition := <-consumer
 	fmt.Println("Producer ", partition, " Consumer ", cPartition)
+}
+
+// CloseProducer Closes the producer
+func CloseProducer(producer sarama.SyncProducer) {
+	fmt.Println("Trying to close Producer")
+	if err := producer.Close(); err != nil {
+		// Should not reach here
+		panic(err)
+	}
+}
+
+// CloseConsumer closes the consumer
+func CloseConsumer(consumer sarama.Consumer) {
+	fmt.Println("Trying to close consumer")
+	if err := consumer.Close(); err != nil {
+		// Should not reach here
+		panic(err)
+	}
 }
--- main.go
+++ main.go
@@ -11,15 +11,23 @@ import (
 
 func main() {
 
-	brokers := []string{"kafka-broker-01.ana.kochava.com:9092", "kafka-broker-02.ana.kochava.com:9092", "kafka-broker-03.ana.kochava.com:9092"}
+	consumerBrokers := []string{"kafka-broker-01.ana.kochava.com:9092", "kafka-broker-02.ana.kochava.com:9092", "kafka-broker-03.ana.kochava.com:9092"}
+	producerBrokers := []string{"104.196.140.55:9092", "104.196.0.83:9092", "104.196.35.215:9092"}
 
 	fmt.Println("Getting the Kafka consumer")
-	consumer, err := GetKafkaConsumer(brokers)
+	consumer, err := GetKafkaConsumer(consumerBrokers)
 	if err != nil {
 		fmt.Println("Unable to create consumer", err)
 	}
 
-	defer consumer.Close()
+	fmt.Println("Getting the Kafka producer")
+	producer, err := GetKafkaProducer(producerBrokers)
+	if err != nil {
+		fmt.Println("Unable to create producer", err)
+	}
+
+	defer CloseConsumer(consumer)
+	defer CloseProducer(producer)
 
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
@@ -29,18 +37,20 @@ func main() {
 	topic := "events"
 
 	for partition := 0; partition < 24; partition++ {
-		producerChan := make(chan string)
+		transferChan := make(chan *sarama.ProducerMessage, 100000)
 
-		partitionConsumer, err := consumer.ConsumePartition(topic, int32(partition), sarama.OffsetOldest)
+		partitionConsumer, err := consumer.ConsumePartition(topic, int32(partition), sarama.OffsetNewest)
 		if err != nil {
 			panic(err)
 		}
 
-		wg.Add(1)
+		wg.Add(2)
 
-		go PullFromTopic(partitionConsumer, producerChan, signals, &wg)
+		go PullFromTopic(partitionConsumer, transferChan, signals, &wg)
 		fmt.Println("Started consumer for partition ", partition)
 
+		go PushToTopic(producer, transferChan, signals, &wg)
+		fmt.Println("Started producer for partition ", partition)
 	}
 
 	wg.Wait()
